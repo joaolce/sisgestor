32fa4756963feafacc5c22207ac4cae211ceb979
João Lúcio Cardozo <joaolce@users.noreply.github.com>
2009-01-07 23:27:34 +0000

    alterações para o banco de dados.

--- implementacao/src/br/com/ucb/sisgestor/entidade/Permissao.java
+++ implementacao/src/br/com/ucb/sisgestor/entidade/Permissao.java
@@ -8,7 +8,6 @@ import java.util.List;
 import javax.persistence.AttributeOverride;
 import javax.persistence.Column;
 import javax.persistence.ManyToMany;
-import javax.persistence.SequenceGenerator;
 import org.hibernate.annotations.Cascade;
 import org.hibernate.annotations.CascadeType;
 
@@ -21,7 +20,6 @@ import org.hibernate.annotations.CascadeType;
 @javax.persistence.Entity
 @javax.persistence.Table(name = "PRM_PERMISSAO")
 @org.hibernate.annotations.Table(appliesTo = "PRM_PERMISSAO")
-@SequenceGenerator(name = "SEQ_PRM", sequenceName = "SEQ_PRM", allocationSize = 10)
 @AttributeOverride(name = "id", column = @Column(name = "PRM_ID", nullable = false))
 public class Permissao extends ObjetoPersistente {
 
