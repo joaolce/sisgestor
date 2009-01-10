d0eb682c683e82bbafb948963c99dfdb15d6fd2d
thiagopirex <thiagopirex@3aaca78c-cabd-11dd-a880-a1081c7ac358>
2009-01-10 15:40:17 +0000

    Configuração do ambiente de desenvolvimento

---
+++ trunk/outros/configuracao.bat
@@ -0,0 +1,69 @@
+@ECHO OFF
+
+echo Iniciando atualizacoes. Aguarde...
+
+IF EXIST %JBOSS_HOME%\server\default\conf\login-config.xml (
+	del "%JBOSS_HOME%"\server\default\conf\login-config.xml
+)
+	
+IF EXIST %JBOSS_HOME%\server\default\conf\sisgestor.keystore (	
+	del "%JBOSS_HOME%"\server\default\conf\sisgestor.keystore
+)
+
+IF EXIST %JBOSS_HOME%\server\default\deploy\sisgestor-ds.xml (
+	del "%JBOSS_HOME%"\server\default\deploy\sisgestor-ds.xml
+)
+	
+IF EXIST %JBOSS_HOME%\server\default\deploy\jboss-web.deployer\server.xml (	
+	del "%JBOSS_HOME%"\server\default\deploy\jboss-web.deployer\server.xml
+)
+
+
+IF EXIST %JBOSS_HOME%\server\default\lib\hibernate-annotations.jar (
+	del "%JBOSS_HOME%"\server\default\lib\hibernate-annotations.jar
+)
+
+IF EXIST %JBOSS_HOME%\server\default\lib\hibernate-commons-annotations-3.1.0.GA.jar (
+	del "%JBOSS_HOME%"\server\default\lib\hibernate-commons-annotations-3.1.0.GA.jar
+)
+
+IF EXIST %JBOSS_HOME%\server\default\lib\hibernate3.jar (
+	del "%JBOSS_HOME%"\server\default\lib\hibernate3.jar
+)
+
+IF EXIST %JBOSS_HOME%\server\default\lib\jcl-over-slf4j-1.5.6.jar (
+	del "%JBOSS_HOME%"\server\default\lib\jcl-over-slf4j-1.5.6.jar
+)
+
+IF EXIST %JBOSS_HOME%\server\default\lib\mysql.jar (
+	del "%JBOSS_HOME%"\server\default\lib\mysql.jar
+)
+
+IF EXIST %JBOSS_HOME%\server\default\lib\slf4j-api-1.5.6.jar (
+	del "%JBOSS_HOME%\server\default\lib\slf4j-api-1.5.6.jar
+)
+
+IF EXIST %JBOSS_HOME%\server\default\lib\slf4j-simple-1.5.6.jar (
+	del "%JBOSS_HOME%"\server\default\lib\slf4j-simple-1.5.6.jar
+)
+
+copy configuracao\login-config.xml "%JBOSS_HOME%"\server\default\conf
+copy configuracao\sisgestor.keystore "%JBOSS_HOME%"\server\default\conf
+copy configuracao\sisgestor-ds.xml "%JBOSS_HOME%"\server\default\deploy
+copy configuracao\server.xml "%JBOSS_HOME%"\server\default\deploy\jboss-web.deployer
+
+copy libs\hibernate-annotations.jar										"%JBOSS_HOME%"\server\default\lib
+copy libs\hibernate-commons-annotations-3.1.0.GA.jar	"%JBOSS_HOME%"\server\default\lib
+copy libs\hibernate3.jar															"%JBOSS_HOME%"\server\default\lib
+copy libs\jcl-over-slf4j-1.5.6.jar										"%JBOSS_HOME%"\server\default\lib
+copy libs\mysql.jar																		"%JBOSS_HOME%"\server\default\lib
+copy libs\slf4j-api-1.5.6.jar													"%JBOSS_HOME%"\server\default\lib
+copy libs\slf4j-simple-1.5.6.jar											"%JBOSS_HOME%"\server\default\lib
+
+
+
+echo Atualizacoes realizadas com sucesso. 
+
+@ECHO ON
+pause
+
---
+++ trunk/outros/db_sisgestor.sql
@@ -0,0 +1,92 @@
+
+/*!40100 SET CHARACTER SET latin1;*/
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='ANSI';*/
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;*/
+
+
+#
+# Database structure for database 'sisgestor'
+#
+
+CREATE DATABASE /*!32312 IF NOT EXISTS*/ "sisgestor" /*!40100 DEFAULT CHARACTER SET latin1 */;
+
+USE "sisgestor";
+
+
+#
+# Table structure for table 'dpr_departamento'
+#
+
+CREATE TABLE /*!32312 IF NOT EXISTS*/ "dpr_departamento" (
+  "DPR_ID" int(11) NOT NULL AUTO_INCREMENT,
+  "DPR_DESCRICAO" varchar(50) NOT NULL,
+  "DPR_EMAIL" varchar(30) DEFAULT NULL,
+  "DPR_SIGLA" char(10) NOT NULL,
+  "DPR_ID_SUPERIOR" int(11) DEFAULT NULL,
+  PRIMARY KEY ("DPR_ID"),
+  UNIQUE KEY "DPR_SIGLA" ("DPR_SIGLA"),
+  KEY "IR_DPR_DPR" ("DPR_ID_SUPERIOR"),
+  CONSTRAINT "IR_DPR_DPR" FOREIGN KEY ("DPR_ID_SUPERIOR") REFERENCES "dpr_departamento" ("DPR_ID")
+) AUTO_INCREMENT=2 /*!40100 DEFAULT CHARSET=latin1*/;
+
+
+
+#
+# Table structure for table 'prm_permissao'
+#
+
+CREATE TABLE /*!32312 IF NOT EXISTS*/ "prm_permissao" (
+  "PRM_ID" int(11) NOT NULL AUTO_INCREMENT,
+  "PRM_DESCRICAO" varchar(30) NOT NULL,
+  PRIMARY KEY ("PRM_ID")
+) AUTO_INCREMENT=2 /*!40100 DEFAULT CHARSET=latin1*/;
+
+
+
+#
+# Table structure for table 'upm_usuario_permissao'
+#
+
+CREATE TABLE /*!32312 IF NOT EXISTS*/ "upm_usuario_permissao" (
+  "UUR_ID" int(11) NOT NULL,
+  "PRM_ID" int(11) NOT NULL,
+  KEY "FK7972EB89EA570FD8" ("PRM_ID"),
+  KEY "FK7972EB897011A178" ("UUR_ID"),
+  CONSTRAINT "FK7972EB897011A178" FOREIGN KEY ("UUR_ID") REFERENCES "uur_usuario" ("UUR_ID"),
+  CONSTRAINT "FK7972EB89EA570FD8" FOREIGN KEY ("PRM_ID") REFERENCES "prm_permissao" ("PRM_ID")
+) /*!40100 DEFAULT CHARSET=latin1*/;
+
+
+
+#
+# Table structure for table 'uur_usuario'
+#
+
+CREATE TABLE /*!32312 IF NOT EXISTS*/ "uur_usuario" (
+  "UUR_ID" int(11) NOT NULL AUTO_INCREMENT,
+  "UUR_LOGIN" char(15) NOT NULL,
+  "UUR_NOME" varchar(150) NOT NULL,
+  "UUR_SENHA" varchar(255) NOT NULL,
+  "DPR_ID" int(11) NOT NULL,
+  PRIMARY KEY ("UUR_ID"),
+  UNIQUE KEY "UUR_LOGIN" ("UUR_LOGIN"),
+  KEY "IR_DPR_UUR" ("DPR_ID"),
+  CONSTRAINT "IR_DPR_UUR" FOREIGN KEY ("DPR_ID") REFERENCES "dpr_departamento" ("DPR_ID")
+) AUTO_INCREMENT=2 /*!40100 DEFAULT CHARSET=latin1*/;
+
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE;*/
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS;*/
+
+
+INSERT INTO `dpr_departamento` (`DPR_ID`,`DPR_DESCRICAO`,`DPR_EMAIL`,`DPR_SIGLA`,`DPR_ID_SUPERIOR`) 
+	VALUES (1,'Departamento','email@mail.com','DP',NULL);
+
+INSERT INTO `uur_usuario` (`UUR_ID`,`UUR_LOGIN`,`UUR_NOME`,`UUR_SENHA`,`DPR_ID`) 
+	VALUES (1,'admin','Administrador','admin',1);
+
+INSERT INTO `prm_permissao` (`PRM_ID`,`PRM_DESCRICAO`) 
+	VALUES (1,'1');
+
+INSERT INTO `upm_usuario_permissao` (`UUR_ID`,`PRM_ID`) 
+	VALUES (1,1);
