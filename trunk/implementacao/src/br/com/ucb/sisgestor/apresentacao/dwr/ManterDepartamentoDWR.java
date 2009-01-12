d6fcfd930e9ee3b31cbc3cbb49db3e274a56df6c
gustavotavaresdias <gustavotavaresdias@3aaca78c-cabd-11dd-a880-a1081c7ac358>
2009-01-12 22:33:53 +0000

    Atualização de JavaDoc

--- trunk/implementacao/src/br/com/ucb/sisgestor/apresentacao/dwr/ManterDepartamentoDWR.java
+++ trunk/implementacao/src/br/com/ucb/sisgestor/apresentacao/dwr/ManterDepartamentoDWR.java
@@ -4,70 +4,70 @@
  */
 package br.com.ucb.sisgestor.apresentacao.dwr;
 
-import java.util.List;
-
 import br.com.ucb.sisgestor.entidade.Departamento;
 import br.com.ucb.sisgestor.negocio.DepartamentoBO;
 import br.com.ucb.sisgestor.negocio.impl.DepartamentoBOImpl;
 import br.com.ucb.sisgestor.util.ListaResultadoDTO;
 import br.com.ucb.sisgestor.util.constantes.DadosContexto;
+import java.util.List;
 
 /**
  * Objeto ManterDepartamentoDWR do projeto.
- *
+ * 
  * @author Thiago
  * @since 10/01/2009
  */
-public class ManterDepartamentoDWR extends BaseDWR{
-	
+public class ManterDepartamentoDWR extends BaseDWR {
+
 	/**
-	 * Pesquisar os departamentos com os parâmetros preenchidos
-	 * 
-	 * @param nome
-	 * @param login
-	 * @return List
-	 */
-	public ListaResultadoDTO pesquisar(String nome, Integer paginaAtual) {
-		DepartamentoBO departamentoBO = DepartamentoBOImpl.getInstancia();
-		
-		List listaDepartamentos = departamentoBO.getByNome(nome, paginaAtual);
-		
-		ListaResultadoDTO dto = new ListaResultadoDTO();
-		
-		dto.setColecaoParcial(listaDepartamentos);
-		
-		//Busca o total de registros
-		if(paginaAtual == null && !listaDepartamentos.isEmpty()){
-			Integer total = departamentoBO.getTotalRegistros(nome);
-			setSessionAttribute(DadosContexto.TOTAL_PESQUISA_SESSAO, total);
-			dto.setTotalRegistros(total);
-		}else{
-			dto.setTotalRegistros((Integer) getSessionAttribute(DadosContexto.TOTAL_PESQUISA_SESSAO));
-		}
-	
-		return dto;
-	}
-	
-	/**
-	 * Pesquisa o {@link Departamento} pelo id 
+	 * Pesquisa o {@link Departamento} pelo id
 	 * 
 	 * @param idDepartamento
 	 * @return Departamento
 	 */
-	public Departamento getById(Integer idDepartamento){
+	public Departamento getById(Integer idDepartamento) {
 		DepartamentoBO departamentoBO = DepartamentoBOImpl.getInstancia();
 		return departamentoBO.obter(idDepartamento);
 	}
-	
+
 	/**
 	 * 
 	 * Obtem um {@link List} de todos {@link Departamento}
-	 *
-	 * @return
+	 * 
+	 * @return {@link List} de {@link Departamento}
 	 */
-	public List<Departamento> obterTodos(){
+	public List<Departamento> obterTodos() {
 		DepartamentoBO departamentoBO = DepartamentoBOImpl.getInstancia();
 		return departamentoBO.obterTodos();
+	}
+
+	/**
+	 * Pesquisar os departamentos com os parâmetros preenchidos
+	 * 
+	 * @param nome
+	 * @param paginaAtual
+	 * @return List
+	 */
+	public ListaResultadoDTO pesquisar(String nome, Integer paginaAtual) {
+		DepartamentoBO departamentoBO = DepartamentoBOImpl.getInstancia();
+
+		List listaDepartamentos = departamentoBO.getByNome(nome, paginaAtual);
+
+		ListaResultadoDTO dto = new ListaResultadoDTO();
+
+		dto.setColecaoParcial(listaDepartamentos);
+
+		//Busca o total de registros
+		if ((paginaAtual == null) && !listaDepartamentos.isEmpty()) {
+			Integer total = departamentoBO.getTotalRegistros(nome);
+			this.setSessionAttribute(DadosContexto.TOTAL_PESQUISA_SESSAO, total);
+			dto.setTotalRegistros(total);
+		} else {
+			dto.setTotalRegistros((Integer) this
+					.getSessionAttribute(DadosContexto.TOTAL_PESQUISA_SESSAO));
+		}
+
+		return dto;
 	}
 
 }
--- trunk/implementacao/src/br/com/ucb/sisgestor/persistencia/DepartamentoDAO.java
+++ trunk/implementacao/src/br/com/ucb/sisgestor/persistencia/DepartamentoDAO.java
@@ -4,9 +4,8 @@
  */
 package br.com.ucb.sisgestor.persistencia;
 
-import java.util.List;
-
 import br.com.ucb.sisgestor.entidade.Departamento;
+import java.util.List;
 
 /**
  * Interface para acesso aos dados de {@link Departamento}.
@@ -18,18 +17,18 @@ public interface DepartamentoDAO extends BaseDAO<Departamento, Integer> {
 
 	/**
 	 * Retorna um {@link List} de {@link Departamento} a partir da descrição
-	 *
+	 * 
 	 * @param nome
 	 * @param paginaAtual
-	 * @return
+	 * @return {@link List} de {@link Departamento}
 	 */
 	List<Departamento> getByNome(String nome, Integer paginaAtual);
 
 	/**
 	 * Recupera o total de registros retornados pela consulta.
-	 *
+	 * 
 	 * @param nome
-	 * @return
+	 * @return totalRegistros
 	 */
 	Integer getTotalRegistros(String nome);
 }
