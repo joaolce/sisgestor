b7567991dff32c71b38103353a3f2edf21b9afe6
João Lúcio Cardozo <joaolce@users.noreply.github.com>
2009-01-17 20:41:47 +0000

    validações para manter usuário

---
+++ implementacao/src/br/com/ucb/sisgestor/util/constantes/ConstantesRoles.java
@@ -0,0 +1,21 @@
+/*
+ * Projeto: sisgestor
+ * Criação: 17/01/2009 por João Lúcio
+ */
+package br.com.ucb.sisgestor.util.constantes;
+
+/**
+ * Constantes com as roles da aplicação.
+ * 
+ * @author João Lúcio
+ * @since 17/01/2009
+ */
+public interface ConstantesRoles {
+
+	/** Permissão Minima do sistema */
+	public static String	PERMISSAO_MINIMA		= "1";
+	/** Manter Departamento */
+	public static String	MANTER_DEPARTAMENTO	= "2";
+	/** Manter Usuário */
+	public static String	MANTER_USUARIO			= "3";
+}
