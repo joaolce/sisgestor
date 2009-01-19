3435ef49642f1b72ee4800d72d0effde45ba4280
gustavotavaresdias <gustavotavaresdias@3aaca78c-cabd-11dd-a880-a1081c7ac358>
2009-01-19 22:46:49 +0000

    Alterações de manter usuário

--- trunk/implementacao/webapp/js/manterUsuario/permissao.js
+++ trunk/implementacao/webapp/js/manterUsuario/permissao.js
@@ -10,44 +10,47 @@ Event.observe(window, "load", function() {
  */
 var ComportamentosTela = Class.create();
 ComportamentosTela.prototype = {
-	/**
+   /**
 	 * @constructor
 	 */
-	initialize : function() {},
-   
+   initialize : function() {},
+
    /**
 	 * transfere todos os ítens (Permissão) de um combo para o outro e vice-versa
-	 *  
+	 * 
 	 * @param {String} origem
 	 * @param {String} destino
 	 */
-	transfereTodos: function(origem, destino){
-		
-		ComboFunctions.transfereTodos(origem, destino);
-		
-	},
-	
-	/**
+   transfereTodos : function(origem, destino) {
+
+	   ComboFunctions.transfereTodos(origem, destino);
+	   console.debug("transfere 2");
+
+   },
+
+   /**
 	 * Transfere as permissões de um combo para o outro onde ficarão os ítens selecionados
 	 * 
 	 * @param {String} origem
 	 * @param {String} destino
 	 */
-	transferePermissao: function(origem, destino){
-	
-		ComboFunctions.transfereOptions(origem, destino);
-	},
+   transferePermissao : function(origem, destino) {
+
+	   ComboFunctions.transfereOptions(origem, destino);
+	   console.debug("transfere");
+   },
 
    tabelaTelaPrincipal :null,
 
    /**
-    * Recupera todas permissões.
-    * @return
-    */
-   getTodasPermissoes: function(){
-	   ManterUsuarioDWR.getTodasPermissoes(function(permissoes){
-		   dwr.util.removeAllOptions("permissoesInformadas");
-		   dwr.util.addOptions("permissoesInformadas", permissoes, "id", "descricao");
+	 * Recupera todas permissões.
+	 * 
+	 * @return
+	 */
+   getTodasPermissoes : function() {
+	   ManterUsuarioDWR.getTodasPermissoes( function(permissoes) {
+		   dwr.util.removeAllOptions("permissoesInform");
+		   dwr.util.addOptions("permissoesInform", permissoes, "id", "descricao");
 	   });
    }
 };
