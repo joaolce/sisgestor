858b681345d58aff4fd251f02d7051ff6d045d8e
joaolce <joaolce@3aaca78c-cabd-11dd-a880-a1081c7ac358>
2009-04-21 19:18:34 +0000

    enviando email para o novo usuário da tarefa do uso.

--- trunk/implementacao/src/br/com/ucb/sisgestor/util/constantes/Constantes.java
+++ trunk/implementacao/src/br/com/ucb/sisgestor/util/constantes/Constantes.java
@@ -15,21 +15,24 @@ import br.com.ucb.sisgestor.util.DataUtil;
 public final class Constantes {
 
 	/** Versão atual do sistema. */
-	public static final String		VERSAO								= "0.3.0";
+	public static final String		VERSAO									= "0.3.0";
 
 	/** Data da versão atual do sistema. */
-	public static final String		VERSAO_DATA							=
-																							DataUtil.utilDateToString(DataUtil
-																									.getDate(11, 4, 2009));
-
-	/** Assunto do e-mail enviado no lembrete de senha */
-	public static final String		ASSUNTO_EMAIL_LEMBRETE_SENHA	= "SisGestor - Lembrete de senha";
+	public static final String		VERSAO_DATA								=
+																								DataUtil.utilDateToString(DataUtil
+																										.getDate(21, 4, 2009));
 
 	/** Remetente de emails do sisgestor. */
-	public static final String		REMETENTE_EMAIL_SISGESTOR		= "SisGestor";
+	public static final String		REMETENTE_EMAIL_SISGESTOR			= "SisGestor";
+
+	/** Assunto do e-mail enviado no lembrete de senha */
+	public static final String		ASSUNTO_EMAIL_LEMBRETE_SENHA		= "SisGestor - Lembrete de senha";
 
 	/** Assunto do email de novo usuário. */
-	public static final String		ASSUNTO_EMAIL_NOVO_USUARIO		= "SisGestor - Cadastro com sucesso";
+	public static final String		ASSUNTO_EMAIL_NOVO_USUARIO			= "SisGestor - Cadastro com sucesso";
+
+	/** Assunto do email de novo usuário. */
+	public static final String		ASSUNTO_EMAIL_TAREFA_TRANSFERIDA	= "SisGestor - Nova Tarefa";
 
 	/**
 	 * Tamanho do anexo permitido. Ao alterar esta constante, verificar valor correspondente no arquivo
@@ -39,5 +42,5 @@ public final class Constantes {
 	 * 
 	 * Obs.: Atualmente está definido como 10MB
 	 */
-	public static final Integer	TAMANHO_MAX_ANEXO_PERMITIDO	= Integer.valueOf(10 * 1024 * 1024);
+	public static final Integer	TAMANHO_MAX_ANEXO_PERMITIDO		= Integer.valueOf(10 * 1024 * 1024);
 }
