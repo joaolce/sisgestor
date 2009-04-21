425885020e5dadb87193a524904ebf32aad1993f
Thiago Pires <thiagopirex@gmail.com>
2009-04-21 17:03:44 +0000

    Adicionando script para limpar dados do banco.

---
+++ outros/limparBanco.sql
@@ -0,0 +1,16 @@
+use sisgestor;
+
+truncate table anx_anexo;
+truncate table trt_transacao_tarefa;
+truncate table tra_transacao_atividade;
+truncate table trp_transacao_processo;
+
+truncate table huwr_uso_workflow;
+truncate table uca_usoworkflow_campo;
+truncate table uwr_uso_workflow;
+truncate table opc_opcao_campo;
+truncate table cam_campo;
+truncate table tar_tarefa;
+truncate table ati_atividade;
+truncate table pro_processo;
+truncate table wor_workflow;
