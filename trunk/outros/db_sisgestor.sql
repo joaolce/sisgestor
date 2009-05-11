b2ac0225bafbdf57aefa3b881da2b6df038e80af
joaolce <joaolce@3aaca78c-cabd-11dd-a880-a1081c7ac358>
2009-05-11 16:34:45 +0000

    adicionado o modelo_físico, e atualizado a ddl de geração do banco.

--- trunk/outros/db_sisgestor.sql
+++ trunk/outros/db_sisgestor.sql
@@ -1,88 +1,416 @@
-# HeidiSQL Dump 
-#
-# --------------------------------------------------------
-# Host:                 127.0.0.1
-# Database:             sisgestor
-# Server version:       5.1.22-rc-community
-# Server OS:            Win32
-# Target-Compatibility: Standard ANSI SQL
-# HeidiSQL version:     3.2 Revision: 1129
-# --------------------------------------------------------
-
-/*!40100 SET CHARACTER SET latin1;*/
-/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='ANSI';*/
-/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;*/
+/*==============================================================*/
+/* DBMS name:      MySQL 5.0                                    */
+/* Created on:     11/5/2009 12:36:18                           */
+/*==============================================================*/
 
 
-#
-# Dumping data for table 'dpr_departamento'
-#
+drop table if exists sisgestor.ANX_ANEXO;
 
-LOCK TABLES "dpr_departamento" WRITE;
-/*!40000 ALTER TABLE "dpr_departamento" DISABLE KEYS;*/
-INSERT INTO "dpr_departamento" ("DPR_ID", "DPR_EMAIL", "DPR_NOME", "DPR_SIGLA", "DPR_ID_SUPERIOR") VALUES
-	(1,'email@mail.com','Departamento','Dep',NULL);
-/*!40000 ALTER TABLE "dpr_departamento" ENABLE KEYS;*/
-UNLOCK TABLES;
+drop index IX_NOME on sisgestor.ATI_ATIVIDADE;
 
+drop index IX_DESCRICAO on sisgestor.ATI_ATIVIDADE;
 
-#
-# Dumping data for table 'prm_permissao'
-#
+drop table if exists sisgestor.ATI_ATIVIDADE;
 
-LOCK TABLES "prm_permissao" WRITE;
-/*!40000 ALTER TABLE "prm_permissao" DISABLE KEYS;*/
-INSERT INTO "prm_permissao" ("PRM_ID", "PRM_DESCRICAO") VALUES
-	(1,'Acesso mínimo ao SisGestor');
-INSERT INTO "prm_permissao" ("PRM_ID", "PRM_DESCRICAO") VALUES
-	(2,'Manter Departamento');
-INSERT INTO "prm_permissao" ("PRM_ID", "PRM_DESCRICAO") VALUES
-	(3,'Manter Usuário');
-INSERT INTO "prm_permissao" ("PRM_ID", "PRM_DESCRICAO") VALUES
-	(4,'Manter Workflow');
-/*!40000 ALTER TABLE "prm_permissao" ENABLE KEYS;*/
-UNLOCK TABLES;
+drop table if exists sisgestor.CAM_CAMPO;
 
+drop table if exists sisgestor.DPR_DEPARTAMENTO;
 
-#
-# Dumping data for table 'upm_usuario_permissao'
-#
+drop table if exists sisgestor.HUWR_USO_WORKFLOW;
 
-LOCK TABLES "upm_usuario_permissao" WRITE;
-/*!40000 ALTER TABLE "upm_usuario_permissao" DISABLE KEYS;*/
-INSERT INTO "upm_usuario_permissao" ("UUR_ID", "PRM_ID") VALUES	(1,2);
-INSERT INTO "upm_usuario_permissao" ("UUR_ID", "PRM_ID") VALUES	(1,1);
-INSERT INTO "upm_usuario_permissao" ("UUR_ID", "PRM_ID") VALUES	(1,3);
-INSERT INTO "upm_usuario_permissao" ("UUR_ID", "PRM_ID") VALUES	(1,4);
-/*!40000 ALTER TABLE "upm_usuario_permissao" ENABLE KEYS;*/
-UNLOCK TABLES;
+drop table if exists sisgestor.OPC_OPCAO_CAMPO;
 
+drop table if exists sisgestor.PRM_PERMISSAO;
 
-#
-# Dumping data for table 'uur_usuario'
-#
+drop index IX_NOME on sisgestor.PRO_PROCESSO;
 
-LOCK TABLES "uur_usuario" WRITE;
-/*!40000 ALTER TABLE "uur_usuario" DISABLE KEYS;*/
-INSERT INTO "uur_usuario" ("UUR_ID", "UUR_CHEFE", "UUR_EMAIL", "UUR_LOGIN", "UUR_NOME", "UUR_SENHA", "DPR_ID") VALUES
-	(1,'0','','admin','1','1',1);
-/*!40000 ALTER TABLE "uur_usuario" ENABLE KEYS;*/
-UNLOCK TABLES;
+drop index IX_DESCRICAO on sisgestor.PRO_PROCESSO;
 
+drop table if exists sisgestor.PRO_PROCESSO;
 
-#
-# Dumping data for table 'tip_tipo'
-#
+drop index IX_NOME on sisgestor.TAR_TAREFA;
 
-LOCK TABLES "tip_tipo" WRITE;
-/*!40000 ALTER TABLE "tip_tipo" DISABLE KEYS;*/
-INSERT INTO "tip_tipo" ("TIP_ID", "TIP_DESCRICAO") VALUES	(1,'Texto');
-INSERT INTO "tip_tipo" ("TIP_ID", "TIP_DESCRICAO") VALUES	(2,'Data');
-INSERT INTO "tip_tipo" ("TIP_ID", "TIP_DESCRICAO") VALUES	(3,'Hora');
-INSERT INTO "tip_tipo" ("TIP_ID", "TIP_DESCRICAO") VALUES	(4,'Múltipla Escolha');
-INSERT INTO "tip_tipo" ("TIP_ID", "TIP_DESCRICAO") VALUES	(5,'Lista de Opções');
-/*!40000 ALTER TABLE "tip_tipo" ENABLE KEYS;*/
-UNLOCK TABLES;
+drop index IX_DESCRICAO on sisgestor.TAR_TAREFA;
 
-/*!40101 SET SQL_MODE=@OLD_SQL_MODE;*/
-/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS;*/
+drop table if exists sisgestor.TAR_TAREFA;
+
+drop table if exists sisgestor.TRA_TRANSACAO_ATIVIDADE;
+
+drop table if exists sisgestor.TRP_TRANSACAO_PROCESSO;
+
+drop table if exists sisgestor.TRT_TRANSACAO_TAREFA;
+
+drop table if exists sisgestor.UCA_USOWORKFLOW_CAMPO;
+
+drop table if exists sisgestor.UPM_USUARIO_PERMISSAO;
+
+drop table if exists sisgestor.UUR_USUARIO;
+
+drop table if exists sisgestor.UWR_USO_WORKFLOW;
+
+drop index IX_NOME on sisgestor.WOR_WORKFLOW;
+
+drop index IX_DESCRICAO on sisgestor.WOR_WORKFLOW;
+
+drop table if exists sisgestor.WOR_WORKFLOW;
+
+/*==============================================================*/
+/* User: sisgestor                                              */
+/*==============================================================*/
+create user sisgestor;
+
+/*==============================================================*/
+/* Table: ANX_ANEXO                                             */
+/*==============================================================*/
+create table sisgestor.ANX_ANEXO
+(
+   ANX_ID               int not null auto_increment,
+   ANX_CONTENT_TYPE     varchar(50) not null,
+   ANX_DADOS            longblob not null,
+   ANX_DATA_HORA_CRIACAO datetime not null,
+   ANX_NOME             varchar(100) not null,
+   UWR_ID               int not null,
+   primary key (ANX_ID)
+);
+
+/*==============================================================*/
+/* Table: ATI_ATIVIDADE                                         */
+/*==============================================================*/
+create table sisgestor.ATI_ATIVIDADE
+(
+   ATI_ID               int not null auto_increment,
+   ATI_DESCRICAO        varchar(200) not null,
+   ATI_NOME             varchar(100) not null,
+   ATI_LEFT             int,
+   ATI_TOP              int,
+   DRP_ID               int not null,
+   PRO_ID               int not null,
+   primary key (ATI_ID)
+);
+
+/*==============================================================*/
+/* Index: IX_DESCRICAO                                          */
+/*==============================================================*/
+create index IX_DESCRICAO on sisgestor.ATI_ATIVIDADE
+(
+   ATI_DESCRICAO
+);
+
+/*==============================================================*/
+/* Index: IX_NOME                                               */
+/*==============================================================*/
+create index IX_NOME on sisgestor.ATI_ATIVIDADE
+(
+   ATI_NOME
+);
+
+/*==============================================================*/
+/* Table: CAM_CAMPO                                             */
+/*==============================================================*/
+create table sisgestor.CAM_CAMPO
+(
+   CAM_ID               int not null auto_increment,
+   CAM_DESCRICAO        varchar(200),
+   CAM_NOME             varchar(100) not null,
+   CAM_OBRIGATORIO      bit(1) not null,
+   CAM_TIPO             smallint not null,
+   WOR_ID               int not null,
+   primary key (CAM_ID)
+);
+
+/*==============================================================*/
+/* Table: DPR_DEPARTAMENTO                                      */
+/*==============================================================*/
+create table sisgestor.DPR_DEPARTAMENTO
+(
+   DPR_ID               int not null auto_increment,
+   DPR_DATA_HORA_EXCLUSAO datetime,
+   DPR_EMAIL            varchar(50),
+   DPR_NOME             varchar(100) not null,
+   DPR_SIGLA            char(10) not null,
+   DPR_ID_SUPERIOR      int,
+   primary key (DPR_ID)
+);
+
+/*==============================================================*/
+/* Table: HUWR_USO_WORKFLOW                                     */
+/*==============================================================*/
+create table sisgestor.HUWR_USO_WORKFLOW
+(
+   HUWR_DATA_HORA       datetime not null,
+   UWR_ID               int not null,
+   UUR_ID               int not null,
+   HUWR_ACAO            smallint not null,
+   primary key (HUWR_DATA_HORA, UWR_ID)
+);
+
+/*==============================================================*/
+/* Table: OPC_OPCAO_CAMPO                                       */
+/*==============================================================*/
+create table sisgestor.OPC_OPCAO_CAMPO
+(
+   OPC_ID               int not null auto_increment,
+   OPC_DESCRICAO        char(20) not null,
+   OPC_VALOR            int not null,
+   CAM_ID               int not null,
+   primary key (OPC_ID)
+);
+
+/*==============================================================*/
+/* Table: PRM_PERMISSAO                                         */
+/*==============================================================*/
+create table sisgestor.PRM_PERMISSAO
+(
+   PRM_ID               int not null auto_increment,
+   PRM_DESCRICAO        varchar(200) not null,
+   primary key (PRM_ID)
+);
+
+/*==============================================================*/
+/* Table: PRO_PROCESSO                                          */
+/*==============================================================*/
+create table sisgestor.PRO_PROCESSO
+(
+   PRO_ID               int not null auto_increment,
+   PRO_DESCRICAO        varchar(200) not null,
+   PRO_NOME             varchar(100) not null,
+   PRO_LEFT             int,
+   PRO_TOP              int,
+   WOR_ID               int not null,
+   primary key (PRO_ID)
+);
+
+/*==============================================================*/
+/* Index: IX_DESCRICAO                                          */
+/*==============================================================*/
+create index IX_DESCRICAO on sisgestor.PRO_PROCESSO
+(
+   PRO_DESCRICAO
+);
+
+/*==============================================================*/
+/* Index: IX_NOME                                               */
+/*==============================================================*/
+create index IX_NOME on sisgestor.PRO_PROCESSO
+(
+   PRO_NOME
+);
+
+/*==============================================================*/
+/* Table: TAR_TAREFA                                            */
+/*==============================================================*/
+create table sisgestor.TAR_TAREFA
+(
+   TAR_ID               int not null auto_increment,
+   TAR_DESCRICAO        varchar(200) not null,
+   TAR_NOME             varchar(100) not null,
+   TAR_LEFT             int,
+   TAR_TOP              int,
+   ATI_ID               int not null,
+   UUR_ID               int,
+   primary key (TAR_ID)
+);
+
+/*==============================================================*/
+/* Index: IX_DESCRICAO                                          */
+/*==============================================================*/
+create index IX_DESCRICAO on sisgestor.TAR_TAREFA
+(
+   TAR_DESCRICAO
+);
+
+/*==============================================================*/
+/* Index: IX_NOME                                               */
+/*==============================================================*/
+create index IX_NOME on sisgestor.TAR_TAREFA
+(
+   TAR_NOME
+);
+
+/*==============================================================*/
+/* Table: TRA_TRANSACAO_ATIVIDADE                               */
+/*==============================================================*/
+create table sisgestor.TRA_TRANSACAO_ATIVIDADE
+(
+   TRA_ID               int not null auto_increment,
+   ATI_ID_ANTERIOR      int not null,
+   ATI_ID_POSTERIOR     int not null,
+   primary key (TRA_ID)
+);
+
+/*==============================================================*/
+/* Table: TRP_TRANSACAO_PROCESSO                                */
+/*==============================================================*/
+create table sisgestor.TRP_TRANSACAO_PROCESSO
+(
+   TRP_ID               int not null auto_increment,
+   PRO_ID_ANTERIOR      int not null,
+   PRO_ID_POSTERIOR     int not null,
+   primary key (TRP_ID)
+);
+
+/*==============================================================*/
+/* Table: TRT_TRANSACAO_TAREFA                                  */
+/*==============================================================*/
+create table sisgestor.TRT_TRANSACAO_TAREFA
+(
+   TRT_ID               int not null auto_increment,
+   TAR_ID_ANTERIOR      int not null,
+   TAR_ID_POSTERIOR     int not null,
+   primary key (TRT_ID)
+);
+
+/*==============================================================*/
+/* Table: UCA_USOWORKFLOW_CAMPO                                 */
+/*==============================================================*/
+create table sisgestor.UCA_USOWORKFLOW_CAMPO
+(
+   UCA_ID               int not null auto_increment,
+   UCA_VALOR            varchar(100) not null,
+   CAM_ID               int not null,
+   UWR_ID               int not null,
+   primary key (UCA_ID)
+);
+
+/*==============================================================*/
+/* Table: UPM_USUARIO_PERMISSAO                                 */
+/*==============================================================*/
+create table sisgestor.UPM_USUARIO_PERMISSAO
+(
+   UUR_ID               int not null,
+   PRM_ID               int not null,
+   primary key (UUR_ID, PRM_ID)
+);
+
+/*==============================================================*/
+/* Table: UUR_USUARIO                                           */
+/*==============================================================*/
+create table sisgestor.UUR_USUARIO
+(
+   UUR_ID               int not null auto_increment,
+   UUR_CHEFE            bit(1) not null,
+   UUR_DATA_HORA_EXCLUSAO datetime,
+   UUR_EMAIL            varchar(50),
+   UUR_LOGIN            char(15) not null,
+   UUR_NOME             varchar(100) not null,
+   UUR_SENHA            char(20) not null,
+   DPR_ID               int not null,
+   primary key (UUR_ID)
+);
+
+/*==============================================================*/
+/* Table: UWR_USO_WORKFLOW                                      */
+/*==============================================================*/
+create table sisgestor.UWR_USO_WORKFLOW
+(
+   UWR_ID               int not null auto_increment,
+   UWR_ANOTACAO         varchar(500),
+   UWR_DATA_HORA_INICIO datetime,
+   UWR_NUMERO           int not null,
+   UWR_FINALIZADO       bit(1) not null,
+   TAR_ID               int not null,
+   WOR_ID               int not null,
+   primary key (UWR_ID)
+);
+
+/*==============================================================*/
+/* Table: WOR_WORKFLOW                                          */
+/*==============================================================*/
+create table sisgestor.WOR_WORKFLOW
+(
+   WOR_ID               int not null auto_increment,
+   WOR_DESCRICAO        varchar(200) not null,
+   WOR_NOME             varchar(100) not null,
+   WOR_ATIVO            bit(1) not null,
+   WOR_DATA_HORA_EXCLUSAO datetime,
+   primary key (WOR_ID)
+);
+
+/*==============================================================*/
+/* Index: IX_DESCRICAO                                          */
+/*==============================================================*/
+create index IX_DESCRICAO on sisgestor.WOR_WORKFLOW
+(
+   WOR_DESCRICAO
+);
+
+/*==============================================================*/
+/* Index: IX_NOME                                               */
+/*==============================================================*/
+create index IX_NOME on sisgestor.WOR_WORKFLOW
+(
+   WOR_NOME
+);
+
+alter table sisgestor.ANX_ANEXO add constraint IR_UWR_ANX foreign key (UWR_ID)
+      references sisgestor.UWR_USO_WORKFLOW (UWR_ID);
+
+alter table sisgestor.ATI_ATIVIDADE add constraint IR_DPR_ATI foreign key (DRP_ID)
+      references sisgestor.DPR_DEPARTAMENTO (DPR_ID);
+
+alter table sisgestor.ATI_ATIVIDADE add constraint IR_PRO_ATI foreign key (PRO_ID)
+      references sisgestor.PRO_PROCESSO (PRO_ID);
+
+alter table sisgestor.CAM_CAMPO add constraint IR_WOR_CAM foreign key (WOR_ID)
+      references sisgestor.WOR_WORKFLOW (WOR_ID);
+
+alter table sisgestor.DPR_DEPARTAMENTO add constraint IR_DPR_DPR foreign key (DPR_ID_SUPERIOR)
+      references sisgestor.DPR_DEPARTAMENTO (DPR_ID);
+
+alter table sisgestor.HUWR_USO_WORKFLOW add constraint IR_UUR_HUWR foreign key (UUR_ID)
+      references sisgestor.UUR_USUARIO (UUR_ID);
+
+alter table sisgestor.HUWR_USO_WORKFLOW add constraint IR_UWR_HUWR foreign key (UWR_ID)
+      references sisgestor.UWR_USO_WORKFLOW (UWR_ID);
+
+alter table sisgestor.OPC_OPCAO_CAMPO add constraint IR_CAM_OPC foreign key (CAM_ID)
+      references sisgestor.CAM_CAMPO (CAM_ID);
+
+alter table sisgestor.PRO_PROCESSO add constraint IR_WOR_PRO foreign key (WOR_ID)
+      references sisgestor.WOR_WORKFLOW (WOR_ID);
+
+alter table sisgestor.TAR_TAREFA add constraint IR_ATI_TAR foreign key (ATI_ID)
+      references sisgestor.ATI_ATIVIDADE (ATI_ID);
+
+alter table sisgestor.TAR_TAREFA add constraint IR_UUR_TAR foreign key (UUR_ID)
+      references sisgestor.UUR_USUARIO (UUR_ID);
+
+alter table sisgestor.TRA_TRANSACAO_ATIVIDADE add constraint IR_ATI_TRA1 foreign key (ATI_ID_ANTERIOR)
+      references sisgestor.ATI_ATIVIDADE (ATI_ID);
+
+alter table sisgestor.TRA_TRANSACAO_ATIVIDADE add constraint IR_ATI_TRA2 foreign key (ATI_ID_POSTERIOR)
+      references sisgestor.ATI_ATIVIDADE (ATI_ID);
+
+alter table sisgestor.TRP_TRANSACAO_PROCESSO add constraint IR_PRO_TRP1 foreign key (PRO_ID_ANTERIOR)
+      references sisgestor.PRO_PROCESSO (PRO_ID);
+
+alter table sisgestor.TRP_TRANSACAO_PROCESSO add constraint IR_PRO_TRP2 foreign key (PRO_ID_POSTERIOR)
+      references sisgestor.PRO_PROCESSO (PRO_ID);
+
+alter table sisgestor.TRT_TRANSACAO_TAREFA add constraint IR_TAR_TRT1 foreign key (TAR_ID_ANTERIOR)
+      references sisgestor.TAR_TAREFA (TAR_ID);
+
+alter table sisgestor.TRT_TRANSACAO_TAREFA add constraint IR_TAR_TRT2 foreign key (TAR_ID_POSTERIOR)
+      references sisgestor.TAR_TAREFA (TAR_ID);
+
+alter table sisgestor.UCA_USOWORKFLOW_CAMPO add constraint IR_CAM_UCA foreign key (CAM_ID)
+      references sisgestor.CAM_CAMPO (CAM_ID);
+
+alter table sisgestor.UCA_USOWORKFLOW_CAMPO add constraint IR_UWR_UCA foreign key (UWR_ID)
+      references sisgestor.UWR_USO_WORKFLOW (UWR_ID);
+
+alter table sisgestor.UPM_USUARIO_PERMISSAO add constraint IR_PRM_UPM foreign key (PRM_ID)
+      references sisgestor.PRM_PERMISSAO (PRM_ID);
+
+alter table sisgestor.UPM_USUARIO_PERMISSAO add constraint IR_UUR_UPM foreign key (UUR_ID)
+      references sisgestor.UUR_USUARIO (UUR_ID);
+
+alter table sisgestor.UUR_USUARIO add constraint IR_DPR_UUR foreign key (DPR_ID)
+      references sisgestor.DPR_DEPARTAMENTO (DPR_ID);
+
+alter table sisgestor.UWR_USO_WORKFLOW add constraint IR_TAR_UWR foreign key (TAR_ID)
+      references sisgestor.TAR_TAREFA (TAR_ID);
+
+alter table sisgestor.UWR_USO_WORKFLOW add constraint IR_WOR_UWR foreign key (WOR_ID)
+      references sisgestor.WOR_WORKFLOW (WOR_ID);
+
