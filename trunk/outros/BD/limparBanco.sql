1f0120ddb8fdcdbe5d7b84177842b4b9f51a8cf2
joaolce <joaolce@3aaca78c-cabd-11dd-a880-a1081c7ac358>
2009-05-27 03:11:39 +0000

    inserido script cargaInicial.sql e atualizado limparBanco.sql

--- trunk/outros/BD/limparBanco.sql
+++ trunk/outros/BD/limparBanco.sql
@@ -14,3 +14,9 @@ truncate table tar_tarefa;
 truncate table ati_atividade;
 truncate table pro_processo;
 truncate table wor_workflow;
+
+truncate table upm_usuario_permissao;
+truncate table prm_permissao;
+truncate table uur_usuario;
+update dpr_departamento set dpr_departamento.DPR_ID_SUPERIOR = null;
+truncate table dpr_departamento;
